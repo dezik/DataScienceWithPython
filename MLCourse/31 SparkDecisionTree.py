f88f2de38bb3cd5a555e7945b97add8806598d4b
10060618 <10060618@gemalto.com>
2020-02-26 15:55:51 +0100

    spark decision trees

--- MLCourse/31 SparkDecisionTree.py
+++ MLCourse/31 SparkDecisionTree.py
@@ -1,29 +1,36 @@
+from numpy import array
+from pyspark import SparkConf, SparkContext
 from pyspark.mllib.regression import LabeledPoint
 from pyspark.mllib.tree import DecisionTree
-from pyspark import SparkConf, SparkContext
-from numpy import array
+
+#######################################################
+#  Do `spark-submit this_file.py` to run this file    #
+#######################################################
 
 # Boilerplate Spark stuff:
 conf = SparkConf().setMaster("local").setAppName("SparkDecisionTree")
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
+
 
 # Some functions that convert our CSV input data into numerical
 # features for each job candidate
 def binary(YN):
-    if (YN == 'Y'):
+    if YN == 'Y':
         return 1
     else:
         return 0
 
+
 def mapEducation(degree):
-    if (degree == 'BS'):
+    if degree == 'BS':
         return 1
-    elif (degree =='MS'):
+    elif degree == 'MS':
         return 2
-    elif (degree == 'PhD'):
+    elif degree == 'PhD':
         return 3
     else:
         return 0
+
 
 # Convert a list of raw fields from our CSV file to a
 # LabeledPoint that MLLib can use. All data must be numerical...
@@ -37,12 +44,13 @@ def createLabeledPoints(fields):
     hired = binary(fields[6])
 
     return LabeledPoint(hired, array([yearsExperience, employed,
-        previousEmployers, educationLevel, topTier, interned]))
+                                      previousEmployers, educationLevel, topTier, interned]))
 
-#Load up our CSV file, and filter out the header line with the column names
+
+# Load up our CSV file, and filter out the header line with the column names
 rawData = sc.textFile("PastHires.csv")
 header = rawData.first()
-rawData = rawData.filter(lambda x:x != header)
+rawData = rawData.filter(lambda x: x != header)
 
 # Split each line into a list based on the comma delimiters
 csvData = rawData.map(lambda x: x.split(","))
@@ -54,12 +62,12 @@ trainingData = csvData.map(createLabeledPoints)
 # 3 previous employers, a BS degree, but from a non-top-tier school where
 # he or she did not do an internship. You could of course load up a whole
 # huge RDD of test candidates from disk, too.
-testCandidates = [ array([10, 1, 3, 1, 0, 0])]
+testCandidates = [array([10, 1, 3, 1, 0, 0])]
 testData = sc.parallelize(testCandidates)
 
 # Train our DecisionTree classifier using our data set
 model = DecisionTree.trainClassifier(trainingData, numClasses=2,
-                                     categoricalFeaturesInfo={1:2, 3:4, 4:2, 5:2},
+                                     categoricalFeaturesInfo={1: 2, 3: 4, 4: 2, 5: 2},
                                      impurity='gini', maxDepth=5, maxBins=32)
 
 # Now get predictions for our unknown candidates. (Note, you could separate
